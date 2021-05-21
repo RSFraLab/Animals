ef99081e3230dadd54448b6bfd1949979c2d7477
Rupesh Jeyaram <rjeyaram@caltech.edu>
2021-05-20 20:01:20 -0400

    Include animals_color function

--- src/Animals.jl
+++ src/Animals.jl
@@ -10,4 +10,14 @@ function greet(animal_name::String)
     end
 end
 
+function animals_color(animal_name::String)
+    if (animal_name == "Dog")
+        return "Gray"
+    elseif (animal_name == "Cat")
+        return "Black"
+    else
+        return "Brown"
+    end
+end
+
 end # module
